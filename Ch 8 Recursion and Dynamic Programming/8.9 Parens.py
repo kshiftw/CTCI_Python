9931292cc1b592a61ad85cb20fe329bd44ae7f6d
kwong <kirkwong33@gmail.com>
2020-07-17 13:15:59 -0700

    Adding 8.9

---
+++ Ch 8 Recursion and Dynamic Programming/8.9 Parens.py
@@ -0,0 +1,60 @@
+""" Cracking the Coding Interview, 6th Edition - Python Solutions
+8.9 Parens: Implement an algorithm to print all valid (Le., properly opened and closed) combinations
+of n pairs of parentheses.
+EXAMPLE
+Input: 3
+Output: ((())) , (()()), (())(), ()(()), ()()()
+"""
+import unittest
+from unittest import TestCase
+
+
+def parens(n: int) -> list:
+    """ Generate all valid combinations of n pairs of parentheses.
+
+    Algorithm taken from the book's solution.
+
+    Idea:
+    - Keep track of how many left and right parentheses need to be added
+        - Recursively add parentheses until base cases
+
+    Complexity:
+    - Time: O(N * Cat(N))
+        See: https://stackoverflow.com/questions/37385964/time-complexity-for-combination-of-parentheses
+
+    Approach:
+    - Base cases:
+        - If there are no more left parentheses remaining, it is invalid
+        - If we have used up more closing (right) parentheses than opening (left) parentheses, it is invalid
+        - If we have used up all left and right parentheses, it is a valid combination - add it to the combo list
+    - Recursively add left parentheses
+        - Recursively add right parentheses
+    """
+    parens_list = []
+
+    def next_parens(left_remain, right_remain, string):
+        if left_remain < 0 or right_remain < left_remain:
+            return
+        if left_remain == 0 and right_remain == 0:
+            parens_list.append(string)
+            return
+
+        string += '('
+        next_parens(left_remain - 1, right_remain, string)
+
+        string = string[:-1] + ')'
+        next_parens(left_remain, right_remain - 1, string)
+
+    next_parens(n, n, "")
+    return parens_list
+
+
+class TestParens(TestCase):
+    def testA(self):
+        self.assertEqual(parens(3), ["((()))", "(()())", "(())()", "()(())",
+                                     "()()()"])
+        self.assertEqual(parens(2), ["(())", "()()"])
+
+
+if __name__ == "__main__":
+    unittest.main()
